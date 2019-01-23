89a9281d4ef4c0ef65e7aaf94472b6bbe09ce624
DanuAdiP <danu261216@gmail.com>
2019-01-23 21:50:10 +0700

    Commit Pertama

---
+++ Hitung umur.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int thnlhr,thnskrg;
+char nama[30];
+
+cout<<"Masukkan nama anda : ";cin.getline(nama,sizeof(nama));
+cout<<"Masukkan Tahun Lahir Anda : ";cin>>thnlhr;
+cout<<endl;
+cout<<"Hai!!! "<<nama;
+thnskrg=2018-thnlhr;
+cout<<" umur anda saat ini "<<thnskrg<<" tahun";
+}
